df586f729beb7e7ad0fda184e974e1850407cf21
cygan3107 <krzysztof.malon@onet.pl>
2024-06-20 23:03:45 +0200

    conitinue

--- gallery.js
+++ gallery.js
@@ -63,3 +63,53 @@ const images = [
         description: "Lighthouse Coast Sea",
     },
 ];
+
+const galleryRef = document.querySelector("ul.gallery");
+
+const markup = images
+    .map(
+        ({ preview, original, description }) => `<li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</li>`
+    )
+    .join("");
+
+galleryRef.innerHTML = markup;
+
+const instance = basicLightbox.create(`<img width="800" height="600">`, {
+    onShow: () => {
+        document.addEventListener("keydown", onEscClick);
+    },
+    onClose: () => {
+        document.removeEventListener("keydown", onEscClick);
+    },
+});
+const imgLightboxRef = instance.element().querySelector("img");
+
+galleryRef.addEventListener("click", onGalleryClick);
+
+function onGalleryClick(event) {
+    const currentImage = event.target;
+    event.preventDefault();
+
+    if (currentImage.tagName !== "IMG") {
+        return;
+    }
+
+    imgLightboxRef.src = `${currentImage.dataset.source}`;
+
+    instance.show();
+}
+
+function onEscClick(event) {
+    if (event.code === "Escape") {
+        instance.close();
+    }
+}
